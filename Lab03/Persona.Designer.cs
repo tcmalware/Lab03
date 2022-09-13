660e0ed9801fcaf40ba181e58f28142cce5c01bb
tcmalware <genarodvo@gmail.com>
2022-09-13 01:02:54 -0500

    commit3

--- Lab03/Persona.Designer.cs
+++ Lab03/Persona.Designer.cs
@@ -53,6 +53,7 @@
             this.btnBuscar.TabIndex = 1;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // txtNombre
             // 
--- Lab03/Persona.cs
+++ Lab03/Persona.cs
@@ -30,7 +30,7 @@ namespace Lab03
         {
             if (conn.State == ConnectionState.Open)
             {
-                String sql = "SELECT * FROM tbl_usuario";
+                String sql = "SELECT * FROM Person";
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 SqlDataReader reader = cmd.ExecuteReader();
 
@@ -43,6 +43,34 @@ namespace Lab03
             {
                 MessageBox.Show("La conexión esta cerrada");
             }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                String FirstName = txtNombre.Text;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "BuscarPersonaNombre";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@FirstName";
+                param.SqlDbType = SqlDbType.NVarChar;
+                param.Value = FirstName;
+
+                cmd.Parameters.Add(param);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+
+
         }
     }
 }
